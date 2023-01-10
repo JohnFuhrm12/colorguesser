537fe4b5905a2774f20a5b003fc6a4fdd10dd444
JohnFuhrm12 <61069716+JohnFuhrm12@users.noreply.github.com>
2023-01-10 14:41:00 -0300

    Created Square

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [color, setColor] = useState('aqua');
+
+
   return (
-    <div className="App">
-      <h1>Color Guesser</h1>
+    <div className="container">
+      <div className='square' style={{backgroundColor: color}}/>
+      <h1 className='title'>Guess the Color</h1>
     </div>
   );
 }
